1e332e6c3644fc07fb5b0fd2572bf15ae8f21516
Saumya Ranjan <saumyaranjan1111@gmail.com>
2023-12-23 18:15:19 +0530

    changed starter template

---
+++ starterTemplate.cpp
@@ -0,0 +1,21 @@
+
+#include<bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+
+using namespace std;
+using namespace chrono;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll getRandomNumber(ll l, ll r) {return uniform_int_distribution<ll>(l, r)(rng);} 
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+    }
+}
