95242b292c26212c44368cf7d6662965c8239973
Saumya Ranjan <saumyaranjan1111@gmail.com>
2023-12-30 16:50:22 +0530

    added djikstra code

--- starterTemplate.cpp
+++ starterTemplate.cpp
@@ -37,15 +37,38 @@ ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) %
 ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
 
 void _print(vector<ll> &arr){for(auto &x:arr)cout<<x<<" ";cout<<endl;}
+
+void djikstra(ll root, vector<vector<pair<ll, ll>>>& graph, vector<ll> &dist){
+    priority_queue <pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> pq;
+
+    pq.push({0, root});
+    dist[root] = 0;
+    while(!pq.empty()){
+        ll pathwt = pq.top().first;
+        ll node = pq.top().second;
+        pq.pop();
+        for(auto child : graph[node]){
+            ll childnode = child.first;
+            ll edgewt = child.second;
+
+            if(dist[childnode] > dist[node] + edgewt){
+                dist[childnode] = dist[node] + edgewt;
+                pq.push({dist[childnode], childnode});
+            }
+        }
+    }
+}
 /*--------------------------------------------------------------------------------------------------------------------------*/
 
 void solve(){
-    
+    ll t;
+    cin>>t;
+    while(t--){
+
+    }
 }
  
 signed main() {
-
     fastio();
     solve();
-
 }
